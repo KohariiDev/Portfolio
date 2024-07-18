1d538c3d529f30323c01ade4698052e6b637202f
Norbert Kohari <106980094+KohariiDev@users.noreply.github.com>
2024-07-18 16:26:45 +0200

    button color fix

--- src/components/ui/horizontal-components/BackButton.jsx
+++ src/components/ui/horizontal-components/BackButton.jsx
@@ -8,7 +8,7 @@ const BackButton = () => {
   return (
     <Link href="/">
       <Rounded
-        backgroundColor="#ef4444"
+        backgroundColor="#0F172A"
         className="fixed left-10 top-[530px] z-50 flex h-[50px] w-[50px] items-center justify-center rounded-full bg-red-400 tracking-wider text-hero-color lg:top-[720px] lg:h-[50px] lg:w-[50px] xl:top-[625px] 2k:top-[1550px] 2k:w-[120px] 2k:h-[120px]"
       >
         <button className="z-50 uppercase">
--- src/components/ui/horizontal-components/VisitButton.jsx
+++ src/components/ui/horizontal-components/VisitButton.jsx
@@ -6,7 +6,7 @@ const VisitButton = ({project}) => {
   return (
     <Link href={project.url}>
       <Rounded
-        backgroundColor="#ef4444"
+        backgroundColor="#0F172A"
         className="fixed right-10 top-[530px] z-50 flex h-[50px] w-[50px] items-center justify-center rounded-full bg-red-400 tracking-wider text-hero-color lg:top-[720px] lg:h-[50px] lg:w-[50px] xl:top-[625px] 2k:top-[1550px] 2k:h-[120px] 2k:w-[120px]"
       >
         <button className="z-50 uppercase">VISIT</button>
